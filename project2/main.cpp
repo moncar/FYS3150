af38441a0bdb172c2accd15b84a054792082ec63
Filiphl <filiphenriklarsen@gmail.com>
2014-09-26 12:05:05 +0200

    klar for levering

--- project2/main.cpp
+++ project2/main.cpp
@@ -66,7 +66,7 @@ void rotate(mat& A, mat& R, int k, int l, int n)
     return;
 }
 
-mat Jacobi(mat A, int n)
+mat Jacobi(mat A, int n, vec p)
 {
     //Setting up eigenvector matrix
     mat R = zeros<mat>(n,n);
@@ -81,15 +81,51 @@ mat Jacobi(mat A, int n)
     double max_offdiag = max_offdiagonal(n, A, k, l); //Stores the largest offdiagonal elementvalue.
 
     while (fabs(max_offdiag)>epsilon && (double) iterations < max_iterations){
-        max_offdiag = max_offdiagonal(n,A,k,l); //Changes k,l and max_offdiag.
         rotate(A,R,k,l,n);  //Does the rotation turning A(k,l) to zero.
+        max_offdiag = max_offdiagonal(n,A,k,l); //Changes k,l and max_offdiag.
         iterations++;
     }
 
-    //Plot
+    vec D2 = sort(A.diag());
+    vec R0, R1, R2;
+
+    for (int k=0;k<n;k++){
+        for (int l=0;l<n;l++){
+            if (A(k,l) == D2(0)){
+                R0 = R.col(l);
+            }
+            if (A(k,l) == D2(1)){
+                R1 = R.col(l);
+            }
+            if (A(k,l) == D2(2)){
+                R2 = R.col(l);
+            }
+        }
+    }
 
 
+    //Write to file
+    ofstream infile;
+    infile.open ("/home/filiphl/Desktop/c2_r.txt");
+    infile << setw(15)<<setprecision(8)<<"r/alpha";
+    infile << setw(15)<<setprecision(8)<<"R0";
+    infile << setw(15)<<setprecision(8)<<"R1";
+    infile << setw(15)<<setprecision(8)<<"R2";
+    infile << setw(15)<<setprecision(8)<<"R0^2";
+    infile << setw(15)<<setprecision(8)<<"R1^2";
+    infile << setw(15)<<setprecision(8)<<"R2^2" <<endl;
 
+    for (int i=0;i<n;i++){
+        infile << setw(15)<<setprecision(8)<<p(i);
+        infile << setw(15)<<setprecision(8)<<R0(i);
+        infile << setw(15)<<setprecision(8)<<R1(i);
+        infile << setw(15)<<setprecision(8)<<R2(i);
+        infile << setw(15)<<setprecision(8)<<pow(R0(i),2);
+        infile << setw(15)<<setprecision(8)<<pow(R1(i),2);
+        infile << setw(15)<<setprecision(8)<<pow(R2(i),2) <<endl;
+    }
+
+    infile.close();
 
 
 
@@ -108,7 +144,7 @@ mat Jacobi(mat A, int n)
     ALLEREDE NORMERT!!!! <:o
 */
 
-    //cout<<"iterations: "<<iterations<<endl;
+    cout<<"iterations: "<<iterations<<endl;
     return A;
 }
 
@@ -117,91 +153,97 @@ mat Jacobi(mat A, int n)
 
 int main()
 {
-    int n_step = 150;   //280 in b)
-    int p_max = 5;
+    int n_step = 350;   //280 in b)
+    int p_max = 5;     //5 in b)
     double h = (double)p_max/n_step;
+    vec p = linspace(0, p_max, n_step+1);
 
-      /*-------------------------------*/
-//    // Part B
-//    //Setting up the tridiagonal matrix
-//    mat A = zeros<mat>(n_step,n_step);
 
-//    for (int i=1; i<n_step; i++)
-//    {
-//        A(i-1,i-1) = (2/(h*h))+pow((i*h),2);
-//        A(i-1,i) = -1./(h*h);
-//    }
-//    //Adding the last diagonal element.
-//    A(n_step-1,n_step-1)= (2./(h*h))+pow(((n_step)*h),2);
-//    A = symmatu(A); //EXPLAIN THIS IN THE REPORT!
+/*--------------------------------------------------*/
 
-//    //Finding the eigenvalues
-//    mat Eigval = Jacobi(A,n_step);
-//    vec D = sort(Eigval.diag()); //Vector containing the eigenvalues sorted by size.
 
-//    cout<<"Part b) \n\n";
-//    cout<<"lambda0 = " << setw(5) << setprecision(4) << D(0)<<endl;
-//    cout<<"lambda1 = "<< setw(5) << setprecision(4) << D(1)<<endl;
-//    cout<<"lambda2 = "<< setw(5) << setprecision(4) << D(2)<<endl;
+    // Part B
+    //Setting up the tridiagonal matrix
+    mat A = zeros<mat>(n_step,n_step);
+
+    for (int i=1; i<n_step; i++)
+    {
+        A(i-1,i-1) = (2/(h*h))+pow((p(i)),2);
+        A(i-1,i) = -1./(h*h);
+    }
+    //Adding the last diagonal element.
+    A(n_step-1,n_step-1)= (2./(h*h))+pow(p(n_step),2);
+    A = symmatu(A); //EXPLAIN THIS IN THE REPORT!
+
+    //Finding the eigenvalues
+    mat Eigval = Jacobi(A,n_step, p);
+    vec D = sort(Eigval.diag()); //Vector containing the eigenvalues sorted by size.
+
+    cout<<"Part b) \n\n";
+    cout<<"lambda0 = " << setw(5) << setprecision(4) << D(0)<<endl;
+    cout<<"lambda1 = "<< setw(5) << setprecision(4) << D(1)<<endl;
+    cout<<"lambda2 = "<< setw(5) << setprecision(4) << D(2)<<endl;
 
 
 //    /*-----------------------------------------------------------*/
-//    //oppgave c)
+//    //Part C
 //    cout<<"\n\n\nPart c) \n \n";
-//    double w[3];
-//    w[0] = 0.01;
-//    w[1] = 0.5;
-//    w[2] = 1;
-//    n_step = 150;
+//    double w[1];
+//    w[0] = 1./4;
+//    //w[1] = 1./4;
+//    //w[2] = 1;
 
-//    for (int j=0;j<3;j++){
+//    for (int j=0;j<1;j++){
 //        //Setting up the new tridiagonal matrix (with the new potentials)
 //        mat B = zeros<mat>(n_step,n_step);
 
 //        double Vc;
 //        for (int i=1; i<n_step; i++)
 //        {
-//            Vc = pow(w[j],2)*pow((i*h),2) + 1./(i*h);
+//            Vc = pow(w[j],2)*pow((p(i)),2) + 1./(p(i));
 //            B(i-1,i-1) = (2/(h*h))+Vc;
 //            B(i-1,i) = -1./(h*h);
 //        }
 //        //Adding the last diagonal element.
-//        B(n_step-1,n_step-1)= (2/(h*h))+pow(w[j],2)*pow((n_step*h),2) + 1./(n_step*h);
+//        B(n_step-1,n_step-1)= (2/(h*h))+pow(w[j],2)*pow((p(n_step)),2) + 1./p(n_step);
 //        B = symmatu(B); //EXPLAIN THIS IN THE REPORT!
 
 //        //Finding the eigenvalues
-//        mat Eigval2 = Jacobi(B,n_step);
+//        mat Eigval2 = Jacobi(B,n_step, p);
 //        vec D2 = sort(Eigval2.diag()); //Vector containing the eigenvalues sorted by size.
 
-//        cout<< "omega = "<< setw(4) << setprecision(5)<<w[j];
-//        cout<<setw(15)<<"lambda0 = "<< setw(5) << setprecision(5) << D2(0)<<endl;
+//        cout<< "omega = "<< setw(4) << setprecision(4)<<w[j];
+//        cout<<setw(15)<<"lambda0 = "<< setw(5) << setprecision(4) << D2(0)/2<<endl;
 //    }
 
 
     /*---------------------------------------------------*/
-    //Part d)
+//    //Part D
+//    cout<<"\n\n\nPart d) \n \n";
 
-    //Setting up the tridiagonal matrix
-    double w = 0.01;
-    mat A = zeros<mat>(n_step,n_step);
-    double Vd;
-    for (int i=1; i<n_step; i++)
-    {
-        Vd = pow(w,2)*pow((i*h),2); // + 1./(i*h) if with repultion
-        A(i-1,i-1) = (2/(h*h))+Vd;
-        A(i-1,i) = -1./(h*h);
-    }
-    //Adding the last diagonal element.
-    A(n_step-1,n_step-1)= (2./(h*h))+pow(w,2)*pow((n_step*h),2);
-    A = symmatu(A); //EXPLAIN THIS IN THE REPORT!
 
-    //Finding the eigenvalues
-    mat Eigval = Jacobi(A,n_step);
-    vec D = sort(Eigval.diag()); //Vector containing the eigenvalues sorted by size.
 
-    cout<<"lambda0 ="<<setw(5)<<setprecision(4)<<D(0)<<endl;
-    cout<<"lambda1 ="<<setw(5)<<setprecision(4)<<D(1)<<endl;
-    cout<<"lambda2 ="<<setw(5)<<setprecision(4)<<D(2)<<endl;
+//    //Setting up the tridiagonal matrix
+//    double wd = 0.01;
+//    mat A = zeros<mat>(n_step,n_step);
+//    double Vd;
+//    for (int i=1; i<n_step; i++)
+//    {
+//        Vd = pow(wd,2)*pow((p(i)),2) + 1./(p(i));// with repultion
+//        A(i-1,i-1) = (2/(h*h))+Vd;
+//        A(i-1,i) = -1./(h*h);
+//    }
+//    //Adding the last diagonal element.
+//    A(n_step-1,n_step-1)= (2./(h*h))+pow(wd,2)*pow(p(n_step),2) + 1./(p(n_step));
+//    A = symmatu(A); //EXPLAIN THIS IN THE REPORT!
+
+//    //Finding the eigenvalues
+//    mat Eigval = Jacobi(A, n_step, p);
+//    vec D = sort(Eigval.diag()); //Vector containing the eigenvalues sorted by size.
+
+//    cout<<"lambda0 ="<<setw(5)<<setprecision(4)<<D(0)<<endl;
+//    cout<<"lambda1 ="<<setw(5)<<setprecision(4)<<D(1)<<endl;
+//    cout<<"lambda2 ="<<setw(5)<<setprecision(4)<<D(2)<<endl;
 
 
 
